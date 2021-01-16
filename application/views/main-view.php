7b43b6912c7904506a7d4b64476596821c78a38d
Egor <stanbro@ukr.net>
2021-01-17 01:38:16 +0200

    create section with welcome

--- application/views/main-view.php
+++ application/views/main-view.php
@@ -1,1 +1,11 @@
-<h1>Hello</h1>
+<section class="welcome">
+    <div class="title animate__animated animate__fadeInDown animate__slow"><h1>movement</h1></div>
+    <div class="subtitle">Створений для боротьби з малорухливим способом життя серед дітей під час карантину.</div>
+    <div class="sect-move">
+        <div class="animate__animated animate__slow animate__fadeInLeftBig"><p>move</p></div>
+    </div>
+    <div class="subtitle">Без руху гірше виробляються ендорфіни, які стимулюють людей радіти життю. Вже через десять
+        хвилин ви будете в кращому настрою
+    </div>
+</section>
+
