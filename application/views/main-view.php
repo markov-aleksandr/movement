c6e32a77fac3646a688bcc92798b5d22869b7d79
Egor <stanbro@ukr.net>
2021-01-18 15:38:14 +0200

    готовая верстка

--- application/views/main-view.php
+++ application/views/main-view.php
@@ -1,11 +1,45 @@
 <section class="welcome">
-    <div class="title animate__animated animate__fadeInDown animate__slow"><h1>movement</h1></div>
+    <div class="title"><h1>Movement</h1></div>
     <div class="subtitle">Створений для боротьби з малорухливим способом життя серед дітей під час карантину.</div>
     <div class="sect-move">
         <div class="animate__animated animate__slow animate__fadeInLeftBig"><p>move</p></div>
     </div>
     <div class="subtitle">Без руху гірше виробляються ендорфіни, які стимулюють людей радіти життю. Вже через десять
-        хвилин ви будете в кращому настрою
+        хвилин ви будете в кращому настрою.
+    </div>
+</section>
+<section class="second-sect">
+    <div class="task__inner">
+        <div class="title">Інформація про тебе</div>
+        <div class="content">
+            <img src="/images/Android-amico 2.png" alt="">
+            <form action="">
+                <div class="field">
+                    <input type="text" placeholder="Як тебе звати?">
+                    <input type="text" placeholder="Скільки тобі років?">
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
+<section class="task__change">
+    <div class="title">Виберіть набір вправ</div>
+    <div class="form__inner">
+        <form action="">
+            <button>Sit pellentesque molestie</button>
+            <button>Sit pellentesque molestie</button>
+            <button>Sit pellentesque molestie</button>
+            <button>Sit pellentesque molestie</button>
+        </form>
+        <img src="/images/Android-pana 2.png" alt="">
     </div>
 </section>
 
+<section class="start">
+    <div class="start__content">
+        <div class="title">Ви готові почати?</div>
+        <button class="start__btn">
+            Рухнемося
+        </button>
+    </div>
+</section>
