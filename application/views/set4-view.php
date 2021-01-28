85738bb0c4071680e170056000083b396994c06b
Egor <stanbro@ukr.net>
2021-01-28 11:52:12 +0200

    animate

--- application/views/set4-view.php
+++ application/views/set4-view.php
@@ -1,1 +1,42 @@
-4
+<section class="workout container">
+    <div class="title">movement</div>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active" >
+                <video class="embed-responsive-item" autoplay controls src="/images/move/2.webm"></video>
+
+            </div>
+            <div class="carousel-item">
+
+                <video class="embed-responsive-item" controls autoplay src="/images/move/3.webm"></video>
+            </div>
+            <div class="carousel-item">
+
+                <video class="embed-responsive-item" controls autoplay src="/images/move/4.webm"></video>
+            </div>
+            <div class="carousel-item">
+                <video class="embed-responsive-item" autoplay controls src="/images/move/6.webm"></video>
+            </div>
+
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</section>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
--- application/views/workout-view.php
+++ application/views/workout-view.php
@@ -1,5 +1,6 @@
-<form action="train" method="post">
-    <p>Please select your preferred contact method:</p>
+<div class="title">movement</div>
+
+<form class="change" action="train" method="post">
     <div>
         <input type="radio" id="contactChoice1"
                name="workout" value="set1">
@@ -7,17 +8,19 @@
 
         <input type="radio" id="contactChoice2"
                name="workout" value="set2">
-        <label for="contactChoice2">Набір вправ №1</label>
+        <label for="contactChoice2">Набір вправ №2</label>
 
         <input type="radio" id="contactChoice3"
                name="workout" value="set3">
-        <label for="contactChoice3">Набір вправ №1</label>
+        <label for="contactChoice3">Набір вправ №3</label>
 
         <input type="radio" id="contactChoice3"
                name="workout" value="set4">
-        <label for="contactChoice3">Набір вправ №1</label>
+        <label for="contactChoice3">Набір вправ №4</label>
     </div>
     <div>
         <button type="submit">Submit</button>
+        <img class="img_start" src="/images/робот.png" alt="">
+
     </div>
 </form>
